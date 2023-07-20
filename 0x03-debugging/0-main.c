493f7c3c16418953309ad1b9226217ff5a313ac5
Alexa <onabanjotomiwa@gmail.com>
2023-07-20 15:18:24 -0700

    Learning how to debug is fun

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - This is to Test the function if it is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return(0);
+}
