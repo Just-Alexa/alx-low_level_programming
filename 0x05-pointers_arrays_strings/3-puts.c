e97f771c82034dd656a0be9825eff029800f7e57
Alexa <onabanjotomiwa@gmail.com>
2023-07-24 12:47:02 -0700

    Learning about pointers, arrays and strings makes me want to learn more

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * _puts - prints a string, folowed by a new line, to stdout
- * @str: the string to print
- * Return: string and new line
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-void _puts(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
